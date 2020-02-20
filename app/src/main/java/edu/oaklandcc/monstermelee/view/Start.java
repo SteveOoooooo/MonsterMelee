7d5b24dd85a008ca8ec1f0f2fa0e6d933cd8e5c2
oosting.stephen@student.oaklandcc.edu <oosting.stephen@student.oaklandcc.edu>
2020-02-19 21:34:52 -0500

    Updated Manifest and start activity

--- app/src/main/java/edu/oaklandcc/monstermelee/view/Start.java
+++ app/src/main/java/edu/oaklandcc/monstermelee/view/Start.java
@@ -2,6 +2,7 @@ package edu.oaklandcc.monstermelee.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,5 +15,6 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
